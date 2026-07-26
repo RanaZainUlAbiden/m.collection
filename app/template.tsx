b17cbceafc4f2e49ecabfba200e94f25e98796b7
Haseeb <muhammadhaseebhassan23@gmail.com>
2026-07-26 16:47:56 +0500

    style: remove global page transition from landing page

--- app/template.tsx
+++ app/template.tsx
@@ -1,8 +1,15 @@
 "use client";
 
 import { motion } from "framer-motion";
+import { usePathname } from "next/navigation";
 
 export default function Template({ children }: { children: React.ReactNode }) {
+    const pathname = usePathname();
+
+    if (pathname === "/") {
+        return <div className="flex-1 flex flex-col">{children}</div>;
+    }
+
     return (
         <motion.div
             initial={{ opacity: 0, y: 15 }}
